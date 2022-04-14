e6efa8983d51848c5c8d334120a8c5d30817ad7d
Dashang G Makwana <makwana.dashang@gmail.com>
2022-04-14 15:11:57 +0530

    NG Initial commit
    NG Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  language:string="telu";
+   listOfLanguages: string[] =
+   ["Eng","Hindi","spanish"];
+
+  addLanguage() {
+    console.log("sdf");
+    this.listOfLanguages.push(this.language);
+    console.log(this.listOfLanguages);
+  }
+
+  deleteFirst(){
+    this.listOfLanguages.splice(0,1);
+  }
 }
